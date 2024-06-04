d408ac27edef95b2f688cca34fff2c5d2ae6c7da
khrystia-k <kuts.k@ucu.edu.ua>
2024-06-04 16:18:43 +0300

    change model to gpt-4

--- prompt.py
+++ prompt.py
@@ -1,19 +1,23 @@
 COD_SYSTEM_PROMPT = """You will generate increasingly concise, entity-dense summaries of the above article. 
 Repeat the following 2 steps 5 times. 
 Step 1. Identify 1-3 informative entities (";" delimited) from the article which are missing from the previously generated summary. 
-Step 2. Write a new, denser summary of identical length which covers every entity and detail fromthe previous summary plus the Missing Entities. 
+Step 2. Write a new, denser summary of identical length which covers every entity and detail fromthe previous summary plus the Missing Entities.
+
 A missing entity is:
-- relevant to the main story, 
-- specific yet concise (5 words or fewer), 
-- novel (not in the previous summary), 
-- faithful (present in the article), 
-- anywhere (can be located anywhere in the article).
+- Relevant:  to the main story.
+- Specific: descriptive yet concise (5 words or fewer). 
+- Novel: not in the previous summary. 
+- Faithful: present in the Article.
+- Anywhere: located anywhere in the Article.
+
 Guidelines:
 - The first summary should be long (4-5 sentences, ~80 words) yet highly non-specific, containing little information beyond the entities marked as missing. Use overly verbose language and fillers (e.g., "this article discusses") to reach ~80 words.
-- Make every word count: rewrite the previous summary to improve flow and make space for additional entities.
+- Make every word count: re-write the previous summary to improve flow and make space for additional entities.
 - Make space with fusion, compression, and removal of uninformative phrases like "the article discusses".
-- The summaries should become highly dense and concise yet self-contained, i.e., easily understood without the article. 
-- Missing entities can appear anywhere in the new summary.
-- Never drop entities from the previous summary. If space cannot be made, add fewer new entities. 
+- The summar i es shoul d become hi ghl y dense and conci se yet self-contained, e.g., easily understood without the Article.
+- Missing entities can appear anywhere in the newsummary.
+- Never drop entities fromthe previous summary. If space cannot be made, add f ewer new entities.
+
 Remember, use the exact same number of words for each summary.
-Answer in JSON. The JSONshould be a list (length 5) of dictionaries whose keys are "Missing_Entities" and "Denser_Summary"."""
+
+Return a python list with square brackets. Each element should be a python dictionary with two keys: "Missing_Entities" and "Denser_Summary". There should be 5 elements in the list"""
